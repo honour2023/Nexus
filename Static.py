14fff7320892ccabf465afc35dee689e8bef6df3
honour2023 <oladeleibrahim75@gmail.com>
2017-09-21 11:28:47 +0000

    Static.py

--- Static.py
+++ Static.py
@@ -5,37 +5,27 @@
 class Mest:
     fellow_created = 0
 
-    def __init__(self, name):
+    def __init__(self, name, country, fun_facts,):
+        self.country = country
+        self.fun_facts = fun_facts
         self.name = name
         Mest.fellow_created += 1
 
-#  # Name = Mest(name="")
-#     def to_file(self):
-#         return ",".join(self.__dict__.values())
-#     def fellow_list(self):
-#         with open("fellow.txt",'a') as f:
-#             choice = input("select one: '1' '2' '3': ")
-#             Mest('name')
-#             if choice == 1 or 2:
-#                 if len('f') < 4:
-#                     f.write('f' + "\n")
-#                 else:
-#                     return Mest.fellow_created
-#             print("Error! You can't add another name.")
-#
-# name =input("Enter the name of the fellow: ")
-# print(len(name))
-
-for i in range (10):
-    while i <= 3:
-        Mest("name")
+for i in range(10):
+    Mest("name", "country", "fun_facts")
+    if Mest.fellow_created <= 4:
+        name = input("Enter the name of the fellow you want to hire: ").split()
+        country = input("Enter ya country: ").split()
+        fun_facts = input("Enter ya mutherfucking fun_facts: ").split()
         print(Mest.fellow_created)
-        name = input("Enter the name of the fellow you want paid: ")
-        break
+
     else:
-        print("404,Error you can't add another fellow")
+        print(Mest.fellow_created)
+        raise Exception("404,Error you can't add another fellow")
 
 print(Mest.fellow_created)
+
+
 
 
 
